7396f0239f0e8b30190ca233ae13274225bb4dd9
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-01-29 20:41:47 +0000

    Created a Generator using 2 variables

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #1. Create a greeting for your program.
 name=input("Enter your Name: ")
-print(f"Greetings {name}!\nWelcome to the Band Generator Tool")
+print(f"Greetings {name}!\n\nWelcome to the Band Generator Tool\n\n")
 #2. Ask the user for the city that they grew up in.
-city=input("Where did you grow up? ")
+city=input("Where did you grow up?\n")
 #3. Ask the user for the name of a pet.
-petname=input("What is the name of your pet? ")
+petname=input("What is the name of your pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
 print(f"Your Band name shall be: {city} {petname}!!\n")
 #5. Make sure the input cursor shows on a new line:
